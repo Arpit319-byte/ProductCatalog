31e1fc53778a304ad074bcc66222a67631c6df02
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-08 00:27:10 +0530

    Added the GET Api for the Category controller

--- src/main/java/com/example/ProductCatalog/Controller/CategoryController.java
+++ src/main/java/com/example/ProductCatalog/Controller/CategoryController.java
@@ -1,4 +1,32 @@
 package com.example.ProductCatalog.Controller;
 
+import com.example.ProductCatalog.Service.CategoryService;
+import com.example.ProductCatalog.model.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category")
 public class CategoryController {
+
+    private final static Logger logger = LoggerFactory.getLogger(CategoryController.class);
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        logger.info("Getting all categories");
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+
 }

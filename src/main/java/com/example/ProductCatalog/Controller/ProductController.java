2f1a1312b5f1f43d3a50fcef0da7d490bf540b2c
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-07 23:59:19 +0530

    Added the GET APIs for the product Controller

--- src/main/java/com/example/ProductCatalog/Controller/ProductController.java
+++ src/main/java/com/example/ProductCatalog/Controller/ProductController.java
@@ -1,4 +1,39 @@
 package com.example.ProductCatalog.Controller;
 
+import com.example.ProductCatalog.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+
+    private final static Logger logger = LoggerFactory.getLogger(ProductController.class);
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        logger.info("Getting all products");
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        logger.info("Getting product by id: {}", id);
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
 }

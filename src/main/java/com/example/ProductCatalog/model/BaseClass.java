106e35cac1f8857d90ff2b84666fc22525682e5d
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-07 22:19:55 +0530

    Added the Base Class

---
+++ src/main/java/com/example/ProductCatalog/model/BaseClass.java
@@ -0,0 +1,13 @@
+package com.example.ProductCatalog.model;
+
+import java.util.Date;
+
+public class BaseClass {
+
+    private Long id;
+
+    private Date createdDate;
+
+    private Date updatedDate;
+
+}

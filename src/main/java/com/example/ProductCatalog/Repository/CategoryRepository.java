001a976738dceb704ee8f643b07f078138bb69cf
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-08 00:21:50 +0530

    Category Repsoitory extends JPA repository interface

--- src/main/java/com/example/ProductCatalog/Repository/CategoryRepository.java
+++ src/main/java/com/example/ProductCatalog/Repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.example.ProductCatalog.Repository;
 
-public interface CategoryRepository {
+import com.example.ProductCatalog.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }

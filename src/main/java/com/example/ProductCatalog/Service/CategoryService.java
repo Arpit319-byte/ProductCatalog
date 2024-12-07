232c8907f10a23dac1fc514a1cbdf93ce22f0f5a
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-08 00:29:22 +0530

    Added the get all category method in controller service

--- src/main/java/com/example/ProductCatalog/Service/CategoryService.java
+++ src/main/java/com/example/ProductCatalog/Service/CategoryService.java
@@ -1,4 +1,30 @@
 package com.example.ProductCatalog.Service;
 
+import com.example.ProductCatalog.Repository.CategoryRepository;
+import com.example.ProductCatalog.model.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final static Logger logger = LoggerFactory.getLogger(CategoryService.class);
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    public List<Category> getAllCategories() {
+        logger.info("Getting all categories");
+        return categoryRepository.findAll();
+    }
+
 }

20f98d331c5c6b9f29526ef10d2dcd7ab1b3b11d
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-07 22:42:26 +0530

    Added the attributes for the Base class

--- src/main/java/com/example/ProductCatalog/model/BaseClass.java
+++ src/main/java/com/example/ProductCatalog/model/BaseClass.java
@@ -1,13 +1,31 @@
 package com.example.ProductCatalog.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import java.util.Date;
 
-public class BaseClass {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseClass {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
     private Date createdDate;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
     private Date updatedDate;
 
 }

a18deb4391b74ae0def5c8b64c1e0c42cd5c85ed
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-07 22:24:14 +0530

    Added the attributes for the Product class

--- src/main/java/com/example/ProductCatalog/model/Product.java
+++ src/main/java/com/example/ProductCatalog/model/Product.java
@@ -1,4 +1,24 @@
 package com.example.ProductCatalog.model;
 
-public class Product {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Product extends BaseClass {
+
+    private String name;
+    private String description;
+    private String imageUrl;
+    private Double price;
+    private Category category;
 }

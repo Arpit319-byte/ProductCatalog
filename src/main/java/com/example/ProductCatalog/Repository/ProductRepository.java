87180889590f23d70e29baf1a332102907d69fe7
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-08 00:03:04 +0530

    Product Repository extends the JpaRepository

--- src/main/java/com/example/ProductCatalog/Repository/ProductRepository.java
+++ src/main/java/com/example/ProductCatalog/Repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.example.ProductCatalog.Repository;
 
-public interface ProductRepository {
+import com.example.ProductCatalog.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
